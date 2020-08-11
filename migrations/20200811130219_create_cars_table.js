4e1ac2ee419ac63e73519fe04b2588a10421c3f3
Vickie Nelson <vickie.isabel.nelson@gmail.com>
2020-08-11 14:59:10 -0500

    MVPgit add .git add .

--- migrations/20200811130219_create_cars_table.js
+++ migrations/20200811130219_create_cars_table.js
@@ -4,6 +4,20 @@
 //     The critical information for each car is the VIN, make, model, and mileage.
 //     They also track transmission type and status of the title (clean, salvage, etc.), but this information is not always immediately known.
 
-exports.up = function (knex) {};
-
-exports.down = function (knex) {};
+// the changes we want to make
+exports.up = function (knex) {
+  return knex.schema.createTable("cars", (tbl) => {
+    tbl.increments();
+    tbl.text("VIN", 255).notNullable().unique();
+    tbl.string("make", 255).notNullable();
+    tbl.string("model", 255).notNullable();
+    tbl.integer("mileage").notNullable();
+    tbl.string("transmission type", 255);
+    tbl.string("status", 255);
+  });
+};
+// undo the changes
+exports.down = function (knex) {
+  // drop the cars table
+  return knex.schema.dropTableIfExists("cars");
+};
